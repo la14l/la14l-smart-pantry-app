a43c2a9b14882d6931fd37e745777526764faa64
linahafnaoui <linatalalhafnaoui@gmail.com>
2025-11-13 23:50:00 +0400

    Update Item.java

--- src/main/java/pantryAppPackage/Item.java
+++ src/main/java/pantryAppPackage/Item.java
@@ -122,7 +122,7 @@ public class Item {
                 //if the days left between 0 and 15, then its about to expire
                 return daysLeft >= 0 && daysLeft <= 15;
             }
-            //items in the same month
+            //items in different months but could still have a 15 day difference 
             else if (eMonth == tMonth + 1) {
                 int daysLeft = (30 - tDay) + eDay;
                 return daysLeft <= 15;
