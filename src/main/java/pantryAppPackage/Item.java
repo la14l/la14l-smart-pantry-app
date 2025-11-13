6ff1dbd1f7b34ac90765190f8e1d6489ec7623f3
junaid <abdjunaidpersonal@gmail.com>
2025-11-14 00:39:33 +0400

    Conflict resolution

--- src/main/java/pantryAppPackage/Item.java
+++ src/main/java/pantryAppPackage/Item.java
@@ -2,77 +2,140 @@ package pantryAppPackage;
 
 public class Item {
     //attributes
-    private String Item_ID,Item_Name,Item_Category,Item_ExpDate,Item_Unit;
-    private int Item_Quantity,Item_Threshold;
+    private String itemID, itemName, itemCategory, itemExpDate, itemUnit;
+    private int itemQuantity, itemThreshold;
 
-    public Item(){} //default constructor
+    //default constructor
+    public Item() {
+    }
 
     //full constructor
-    public Item(String ID,String name,String category,int quantity,String unit,int threshold,String expd){
-        this.Item_ID= ID;
-        this.Item_Name= name;
-        this.Item_Category= category;
-        this.Item_Quantity= quantity;
-        this.Item_Unit= unit;
-        this.Item_Threshold= threshold;
-        this.Item_ExpDate= expd;
+    public Item(String itemID, String itemName, String itemCategory, int itemQuantity, String itemUnit, int itemThreshold, String itemExpDate) {
+        this.itemID = itemID;
+        this.itemName = itemName;
+        this.itemCategory = itemCategory;
+        this.itemQuantity = itemQuantity;
+        this.itemUnit = itemUnit;
+        this.itemThreshold = itemThreshold;
+        this.itemExpDate = itemExpDate;
     }
+
     //setters and getters
-    public void setItem_Name(String name){
-        this.Item_Name= name;
+    public String getItemName() {
+        return itemName;
     }
-    public String getItem_Name(){
-        return this.Item_Name;
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
     }
-    public void setItem_Category(String category){
-        this.Item_Category= category;
+
+    public String getItemCategory() {
+        return itemCategory;
     }
-    public String getItem_Category(){
-        return this.Item_Category;
+
+    public void setItemCategory(String itemCategory) {
+        this.itemCategory = itemCategory;
     }
-    public void setItem_Quantity(int quantity){
-        this.Item_Quantity= quantity;
+
+    public String getItemExpDate() {
+        return itemExpDate;
     }
-    public int getItem_Quantity(){
-        return this.Item_Quantity;
+
+    public void setItemExpDate(String itemExpDate) {
+        this.itemExpDate = itemExpDate;
     }
-    public void setItem_Unit(String unit){
-        this.Item_Unit= unit;
+
+    public String getItemUnit() {
+        return itemUnit;
     }
-    public String getItem_Unit(){
-        return this.Item_Unit;
+
+    public void setItemUnit(String itemUnit) {
+        this.itemUnit = itemUnit;
     }
-    public void setItem_Threshold(int threshold){
-        this.Item_Threshold= threshold;
+
+    public int getItemQuantity() {
+        return itemQuantity;
     }
-    public int getItem_Threshold(){
-        return this.Item_Threshold;
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
     }
-    public void setItem_ExpDate(String expd){
-        this.Item_ExpDate= expd;
+
+    public int getItemThreshold() {
+        return itemThreshold;
     }
-    public String getItem_ExpDate(){
-        return this.Item_ExpDate;
+
+    public void setItemThreshold(int itemThreshold) {
+        this.itemThreshold = itemThreshold;
     }
-    //only getter for ID because it has to be unique and shouldnt change after creation
-    public String getItem_ID(){
-        return this.Item_ID;
+
+    //only getter for ID because it has to be unique and shouldn't change after creation
+    public String getItemID() {
+        return this.itemID;
     }
 
     //method to check if item is low in stock
-    public boolean isLowStock(){
-//if quantity<threshold return true else false
-        //lina u have to check that the quantities and threshold is not <0 and if its 0 then its not low in stock +check if we need an interface
+    public boolean isLowStock() {
+        //when item quantity is less than a threshold, then item is low in stock
+        return itemQuantity < itemThreshold;
     }
+
     //method to check if an item is about to expire
-    public boolean isAboutToExpire(){
-        //if the item doesnt have expiry date then it should return false
-        //find the number of days between today and the expiry date
-        // if daysleft <=15 and daysleft>=0 then its about to expire
-        //if daysleft<0 then its already expired
-        //if daysleft>15 then not close to expiry
+    public boolean isAboutToExpire(String currentDate) {
+        //if the item doesn't have an expiry date, then it should return false
+        if (itemExpDate == null || itemExpDate.isEmpty()) {
+            return false;
+        }
+
+        //asking user for today's date
+        //Scanner inputDate = new Scanner(System.in);
+        //System.out.print("Enter today's date please (Y/M/D): ");
+        //String currentDate = inputDate.nextLine();
+        //System.out.print("Enter expiry date (Y/M/D): ");
+        //String expDate = input.nextLine();
+
+        //user must enter date in the correct format
+        if (currentDate.length() != 10 || itemExpDate.length() != 10) {
+            System.out.print("Enter correct date format YYYY/MM/DD");
+            return false;
+        }
+
+        //splitting the expiry date and today's date from user output
+        String[] todayParts = currentDate.split("/");
+        String[] expParts = itemExpDate.split("/");
+
+        //getting each part of the date from the string
+        //parsing each string into integer for calculation
+        int tYear = Integer.parseInt(todayParts[0]);
+        int tMonth = Integer.parseInt(todayParts[1]);
+        int tDay = Integer.parseInt(todayParts[2]);
+        int eYear = Integer.parseInt(expParts[0]);
+        int eMonth = Integer.parseInt(expParts[1]);
+        int eDay = Integer.parseInt(expParts[2]);
+
+        //the difference in dates
+        //first case is if the items are made in the same year
+        if (eYear == tYear) {
+            //check if they're in the same month
+            if (eMonth == tMonth) {
+                int daysLeft = eDay - tDay;
+                //if the days left between 0 and 15, then its about to expire
+                return daysLeft >= 0 && daysLeft <= 15;
+            }
+            //items in different months but could still have a 15 day difference
+            else if (eMonth == tMonth + 1) {
+                int daysLeft = (30 - tDay) + eDay;
+                return daysLeft <= 15;
+            }
+        }
+        return false;
     }
 
-
-
+         /*if(Days left<0){
+           System.out.print("already expired");
+           return false;*/
 }
+
+
+
+
