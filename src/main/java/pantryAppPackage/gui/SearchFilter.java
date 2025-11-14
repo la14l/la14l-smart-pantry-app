eff7ee005460b7b5a708c6ec9dde92c1fbb37218
MH613 <brokenscreen313@gmail.com>
2025-11-14 21:35:36 +0400

    Feat: Added Search&Filter Bar

---
+++ src/main/java/pantryAppPackage/gui/SearchFilter.java
@@ -0,0 +1,64 @@
+package pantryAppPackage.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SearchFilter extends JPanel {
+
+    private JTextField searchBar;
+    private JCheckBox aboutToExpireItemsCheckBox, lowStockItemsCheckBox;
+    private final String[] categories = {"Name", "Category"};
+
+    SearchFilter () {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        setBackground(Color.WHITE);
+
+        // Creating two sections
+        JPanel topSection = new JPanel();
+        topSection.setLayout(new BoxLayout(topSection, BoxLayout.X_AXIS));
+        topSection.setBackground(Color.WHITE);
+
+        JPanel bottomSection = new JPanel();
+        bottomSection.setLayout(new BoxLayout(bottomSection, BoxLayout.X_AXIS));
+        bottomSection.setBackground(Color.WHITE);
+
+        // Creating the input components
+        searchBar = new JTextField();
+        JComboBox searchFilter = new JComboBox(categories);
+        searchFilter.setBackground(Color.WHITE);
+
+        JLabel bottomSectionTitle = new JLabel("Filter by");
+        aboutToExpireItemsCheckBox = new JCheckBox("About To Expire");
+        lowStockItemsCheckBox = new JCheckBox("Low Stock Items");
+
+        aboutToExpireItemsCheckBox.setBackground(Color.WHITE);
+        lowStockItemsCheckBox.setBackground(Color.WHITE);
+
+        topSection.add(searchBar);
+        topSection.add(Box.createRigidArea(new Dimension(5, 0)));
+        topSection.add(searchFilter);
+
+        bottomSection.add(bottomSectionTitle);
+        bottomSection.add(Box.createHorizontalGlue());
+        bottomSection.add(aboutToExpireItemsCheckBox);
+        bottomSection.add(Box.createRigidArea(new Dimension(5, 0)));
+        bottomSection.add(lowStockItemsCheckBox);
+
+        add(topSection);
+        add(Box.createRigidArea(new Dimension(0, 10)));
+        add(bottomSection);
+    }
+
+    public JTextField getSearchBar() {return searchBar;}
+    public JCheckBox aboutToExpireItemsCheckBox() {return aboutToExpireItemsCheckBox;}
+    public JCheckBox lowStockItemsCheckBox() {return lowStockItemsCheckBox;}
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Search and Filter");
+        frame.add(new SearchFilter());
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
