28f892a496db24f8932e1fddbe406cdf9d474205
junaid <abdjunaidpersonal@gmail.com>
2025-11-23 13:32:57 +0400

    fix: removed obsolete throws FileNotFoundException from Main class

--- src/main/java/pantryAppPackage/Main.java
+++ src/main/java/pantryAppPackage/Main.java
@@ -2,10 +2,8 @@ package pantryAppPackage;
 
 import pantryAppPackage.backend.MainFrame;
 
-import java.io.FileNotFoundException;
-
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         new MainFrame();
     }
 }
